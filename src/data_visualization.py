fdd3f7f7b0d51f190e2ba59b527a9d5db23f21a6
Hardik Patel <113786016+HardikPatel-DX@users.noreply.github.com>
2024-09-26 20:17:30 -0600

    Update data_visualization.py

--- src/data_visualization.py
+++ src/data_visualization.py
@@ -1,1 +1,13 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def plot_distribution(df):
+    for col in df.select_dtypes(include=['float64', 'int64']).columns:
+        sns.histplot(df[col], kde=True)
+        plt.show()
+
+def plot_correlation_heatmap(df):
+    corr = df.corr()
+    sns.heatmap(corr, annot=True, cmap="coolwarm")
+    plt.show()
 
